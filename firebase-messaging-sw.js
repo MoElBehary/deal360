4f731b2ec26ce7608478ea219254a98b28d4b78f
Mohamed Elbehary <30749351+MoElBehary@users.noreply.github.com>
2021-09-29 23:11:23 +0200

    deal 360 v0.1
    Search, Auth, Map

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+// importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js")
+// importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js")
+// importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js")
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBrVedoDHzkeDGKgS7P85GriD_6mtwyuYs",
+//     authDomain: "push-notification-c41ac.firebaseapp.com",
+//     projectId: "push-notification-c41ac",
+//     storageBucket: "push-notification-c41ac.appspot.com",
+//     messagingSenderId: "261943328491",
+//     appId: "1:261943328491:web:778aaa6b8e519ffa7088ae",
+//     measurementId: "G-PLSC2J044C"
+// };
+// firebase.initializeApp(firebaseConfig)
+// const messaging = firebase.messaging()
